07eb9c066655abdf4a867c8e0634f9cacff6e144
Shinichiro Yabu <scin.ichi@gmail.com>
2018-01-10 02:06:36 +0900

    Add forgotten files.

---
+++ messages/ja/refill.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'Id' => 'ID',
+    'Item id' => '項目ID',
+    'Amount' => '補充数',
+    'Refill date' => '交換日時',
+    'Delete' => '削除',
+    'Delete this item?' => 'この項目を削除してもよろしいですか？',
+    'Refill details' => '補充日時・数量の変更',
+    'Update' => '変更する',
+];
